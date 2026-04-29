ce2d390e6862ef4d31590c541a8ef437fbae9cfb
Paolo Orundes <orundes@gmail.com>
2026-04-29 07:29:14 -0300

    inicializando TP2

---
+++ seminario1/arrejin_bonadeo_orundescardinaliTP2.R
@@ -0,0 +1,8 @@
+# Maestría en Ciencia de Datos - FCyT - UADER
+# Seminario 1. Técnicas de Análisis en Ciencias de Datos
+# Trabajo Práactico 2. Estimación de parámetros. Correlaciones entre variables. Gráficos. Pruebas de Hipóotesis.
+
+
+# MARTIN ARREJIN - PAMELA BONADEO - ORUNDES CARDINALI PAOLO
+#CODIGO EN REPO https://github.com/orundescardinalipaolo/cdd
+

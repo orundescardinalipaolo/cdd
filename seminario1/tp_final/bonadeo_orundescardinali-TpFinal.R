d93f225fd76e6ee94b0fc2ced709e70a1140a439
Paolo Orundés <orundes@gmail.com>
2026-05-28 17:08:35 -0300

    primer commit tp final

---
+++ seminario1/tp_final/bonadeo_orundescardinali-TpFinal.R
@@ -0,0 +1,10 @@
+# Maestría en Ciencia de Datos - FCyT - UADER
+# Seminario 1. Técnicas de Análisis en Ciencias de Datos
+# Trabajo Práactico Final.
+
+
+# PAMELA BONADEO - ORUNDES CARDINALI PAOLO
+# COMENTAR BLOQUES DE CÓDIGO CONTROL+SHITF+C
+# AUTOAJUSTAR TEXTO (QUITAR BARRA HORIZONTAL) TOOLS / GLOBAL OPTIONS / CODE / DISPLAY / SOFT-WRAP SOURCE FILES
+#CODIGO EN REPO https://github.com/orundescardinalipaolo/cdd
+#FUENTE DEL DATASET http://datos.energia.gob.ar/dataset/1c181390-5045-475e-94dc-410429be4b17/archivo/f8dda0d5-2a9f-4d34-b79b-4e63de3995df
